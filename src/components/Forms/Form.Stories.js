617e437974bbfc8a19cca15d9e0a886c93360b0d
Jon <churchjg283@gmail.com>
2020-10-22 15:27:44 -0400

    messing around with Forms but they wont show up on storybook

---
+++ src/components/Forms/Form.Stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Form from './Form'
+
+let options = ["Pizza", "Wings", "Burgers", "Fries", "Yes"]
+
+storiesOf('Form', module)
+
+    //Emails
+    .add('Email Small', () => <Form type="email" placeholder="Email" />)
+    .add('Email Medium', () => <Form type="email" medium placeholder="Email" />)
+    .add('Email Large', () => <Form type="email" large placeholder="Email" />)
+
+    //Selects
+
+    .add('Select Small', () => <Form type="select" options={options} />)
+    .add('Select Medium', () => <Form type="select" medium options={options} />)
+    .add('Select Large', () => <Form type="select" large options={options} />)
+
+    .add('Select Filled Small', () => <Form type="select" filled options={options} />)
+    .add('Select Filled Medium', () => <Form type="select" filled medium options={options} />)
+    .add('Select Filled Large', () => <Form type="select" filled large options={options} />)
---
+++ src/components/Forms/Form.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Form.css'
+
+const Form = (props) => {
+
+    let classes = ["input"]
+
+    if (props.medium) {
+        classes.push("medium")
+    }
+    if (props.large) {
+        classes.push("large")
+    }
+    if(props.filled){
+        classes.push("filled")
+    }
+    if(props.type=="select"){
+        classes.push("select")
+    }
+
+    const emailForm = props => {
+        return <div>
+            <p>{props.placeholder}</p>
+            <input type={props.type} placeholder={props.placeholder} className={classes.join(" ")} />
+        </div>
+    }
+
+    const selectForm = props => {
+        let options = props.options.map(val => {
+            return <option>{val}</option>
+        })
+
+        return <select value={props.value} className={classes.join(" ")}>
+            <option disabled selected hidden>Select</option>
+            {options}
+            </select>
+    }
+    return (
+        <form className="form">
+            {props.type == "email" ? emailForm(props) : null}
+            {props.type == "select" ? selectForm(props) : null}
+        </form>
+    )
+}
+
+export default Form
