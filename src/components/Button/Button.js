ac676d2593ea0ebb10358de4e75aa32aff9deacf
Jon <churchjg283@gmail.com>
2020-10-22 13:30:24 -0400

    basic buttons done

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,9 +2,35 @@ import React from 'react';
 import './Button.css'
 
 // This is a functional component - just sent up a little differently as an arrow function!
-const Button = (props) => (
-    <button className={`button-${props.type}`}>{props.label}</button>
-)
+const Button = (props) => {
+    Button.defaultProps = {
+        type: "default"
+    }
 
+    let types = ["danger", "success", "warning", "primary"]
+    let classList = ["button"]
+
+    if(props.type=="default"){
+        classList.push("button-default")
+    }
+    else{
+        classList.push(`button-${props.type}`)
+    }
+
+    if(props.large){
+        classList.push("large")
+    }
+
+    if(props.clean){
+        classList.push("clean")
+    }
+
+    if(props.flat){
+        classList.push("flat")
+    }
+   return   <button className={classList.join(" ")}>
+        {props.label}
+    </button>
+}
 
 export default Button; 
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,22 +3,53 @@ import { storiesOf } from '@storybook/react';
 
 import Button from './Button';
 
-
-
 storiesOf('Button', module)
-    .add('Primary', () => <Button
-        label="Primary"
-        type="primary"
-    />)
-    .add('Danger', () => <Button 
-    label="Danger"
-    type="danger" 
-    />)
-    .add('Success', () => <Button 
-    label="Success"
-    type="success" 
-    />)
-    .add('Warning', () => <Button 
-    label="Warning" 
-    type="warning" 
-    />)
+.add('Default', () => <Button label="Jon's Button" />) 
+.add('Default Large', () => <Button  large label="Jon's Button" />) 
+
+.add('Default Clean', () => <Button  clean label="Jon's Button" />) 
+.add('Default Clean Large', () => <Button clean large label="Jon's Button" />) 
+
+.add('Default Flat', () => <Button flat label="Jon's Button" />) 
+.add('Default Flat Large', () => <Button flat large label="Jon's Button" />) 
+
+
+//Primary Class
+.add('Primary', () => <Button type="primary" label="Jon's Button" />) 
+.add('Primary Large', () => <Button type="primary"  large label="Jon's Button" />) 
+
+.add('Primary Clean', () => <Button type="primary" clean label="Jon's Button" />) 
+.add('Primary Clean Large', () => <Button type="primary" clean large label="Jon's Button" />) 
+
+.add('Primary Flat', () => <Button type="primary" flat label="Jon's Button" />) 
+.add('Primary Flat Large', () => <Button type="primary" flat large label="Jon's Button" />) 
+
+//Danger Class
+.add('Danger', () => <Button type="danger" label="Jon's Button" />) 
+.add('Danger Large', () => <Button type="danger"  large label="Jon's Button" />) 
+
+.add('Danger Clean', () => <Button type="danger" clean label="Jon's Button" />) 
+.add('Danger Clean Large', () => <Button type="danger" clean large label="Jon's Button" />) 
+
+.add('Danger Flat', () => <Button type="danger" flat label="Jon's Button" />) 
+.add('Danger Flat Large', () => <Button type="danger" flat large label="Jon's Button" />) 
+
+//Succes Class
+.add('Success', () => <Button type="success" label="Jon's Button" />) 
+.add('Success Large', () => <Button type="success"  large label="Jon's Button" />) 
+
+.add('Success Clean', () => <Button type="success" clean label="Jon's Button" />) 
+.add('Success Clean Large', () => <Button type="success" clean large label="Jon's Button" />) 
+
+.add('Success Flat', () => <Button type="success" flat label="Jon's Button" />) 
+.add('Success Flat Large', () => <Button type="success" flat large label="Jon's Button" />) 
+
+//Warning Class
+.add('Warning', () => <Button type="warning" label="Jon's Button" />) 
+.add('Warning Large', () => <Button type="warning"  large label="Jon's Button" />) 
+
+.add('Warning Clean', () => <Button type="warning" clean label="Jon's Button" />) 
+.add('Warning Clean Large', () => <Button type="warning" clean large label="Jon's Button" />) 
+
+.add('Warning Flat ', () => <Button type="warning" flat label="Jon's Button" />) 
+.add('Warning Flat Large', () => <Button type="warning" flat large label="Jon's Button" />)
